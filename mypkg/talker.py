5f160844b7fc95f20e99c9cd996c049560570160
Setsu Ito <setsu.god@gmail.com>
2022-12-08 12:30:44 +0900

    change talker

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -6,17 +6,19 @@ class Talker():
     def __init__(self):
         self.pub = node.create_publisher(Int16, "countup", 10)
         self.n = 0
+        node.create_timer(0.5, self,  cb)
 
-rclpy.init()
-node = Node("talker")
-talker = Talker()
+    def cb(self):
+        msg = Int16()
+        msg.data = talker.n
+        talker.pub.publish(msg)
+        talker.n += 1
 
-def cb():
-    msg = Int16()
-    msg.data = talker.n
-    talker.pub.publish(msg)
-    talker.n += 1
+def main():
+    rclpy.init()
+    node = Node("talker")
+    talker = Talker(node)
+    rclpy.spin(node)
 
-node.create_timer(0.5, cb)
-rclpy.spin(node)
-
+if __name__ == '__main__':
+    main()
